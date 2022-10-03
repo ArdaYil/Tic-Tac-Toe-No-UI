c90258a89cd77d91533ed367a6c8ebeb6c0a834a
Arda Yildirim <ardayildirim05@icloud.com>
2022-10-03 20:31:54 +0200

    Version: 0.1.8

--- source/Game/Game.java
+++ source/Game/Game.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
+public class Game {
+    private static Scanner keyboard = new Scanner(System.in);
+
+    private String[][] config = {
+        {"players", ""};
+        {"difficulty", ""};
+    };
+
+    public void setConfig(config) {
+        while (true) {
+            System.out.println("Enter");
+            if (!this[config]) continue;
 
 
-public static
+        }
+    }
+
+    public void configGame() {
+        setConfig();
+    }
+}
